7af34becead71c8bc9653e935f1557e9e8982a6e
EmersonAhumada <emerson.ahumada@gmail.com>
2021-09-21 23:14:03 +0000

    done

--- src/app.js
+++ src/app.js
@@ -7,5 +7,32 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extention = [".com", ".net", ".us", ".io", ".tk", ".cn", ".org"];
+
+  for (let index = 0; index < pronoun.length; index++) {
+    let pronombre = pronoun[index];
+    for (let i = 0; i < adj.length; i++) {
+      let adjetivo = adj[i];
+      for (let j = 0; j < noun.length; j++) {
+        let sustantivo = noun[j];
+        for (let z = 0; z < extention.length; z++) {
+          let extension = extention[z];
+
+          console.log(pronombre, adjetivo, sustantivo, extension);
+
+          let add = document.createElement("p");
+          const node = document.createTextNode(
+            pronombre + adjetivo + sustantivo + extension
+          );
+          add.appendChild(node);
+
+          let listado = document.getElementById("list");
+          listado.appendChild(add);
+        }
+      }
+    }
+  }
 };
